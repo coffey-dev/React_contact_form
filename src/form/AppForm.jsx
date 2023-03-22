cbb61d1a7345b4e7cd68d4772fa2e4707fead62e
coffey-dev <scoffey@abc.gob.ar>
2023-03-22 10:29:59 -0300

    first commit

---
+++ src/form/AppForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Formik } from 'formik';
+
+function AppForm() {
+  return (
+    <Formik>
+        {
+            
+        }
+    </Formik>
+  )
+}
+
+export default AppForm
